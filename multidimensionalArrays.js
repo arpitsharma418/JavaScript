a7060ea85cd140d1a721d56f70f65bf8fa2d91ad
Arpit Sharma <as2501759@gmail.com>
2026-09-01 14:56:54 +0530

    learn multidimensional arrays

---
+++ multidimensionalArrays.js
@@ -0,0 +1,9 @@
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = [7, 8, 9];
+
+let arrOfArrays = [arr1, arr2, arr3];
+
+let arrOfArraysOfArrays = [arrOfArrays, arrOfArrays, arrOfArrays];
+
+console.log(arrOfArraysOfArrays);
