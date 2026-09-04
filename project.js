0a2e6d21dec139b179436bdcc917bb79cc3824af
Arpit Sharma <as2501759@gmail.com>
2026-09-04 16:34:54 +0530

    Create company product catalog project

---
+++ project.js
@@ -0,0 +1,26 @@
+// Company product catalog
+
+let inventory = [];
+
+let product1 = {
+  name: "product1",
+  model: "21",
+  cost: "50000",
+  quantity: "10",
+};
+let product2 = {
+  name: "product2",
+  model: "22",
+  cost: "34000",
+  quantity: "10",
+};
+let product3 = {
+  name: "product3",
+  model: "40",
+  cost: "10000",
+  quantity: "56",
+};
+
+inventory.push(product1, product2, product3);
+
+console.log(inventory);
